1b30d1f444f59357ce7d05338afc26c11443ca9c
吕泰鹏 <@DESKTOP-P083CPV>
2020-01-02 18:45:00 +0800

    修改项目代码再次提交

--- src/main/java/com/bw/test/Maain.java
+++ src/main/java/com/bw/test/Maain.java
@@ -3,7 +3,7 @@ package com.bw.test;
 public class Maain {
 
 	public static void main(String[] args) {
-		System.out.println("heeelllword");
+		System.out.println("heeelllword+“”“”上课了空间就看见待会上课US放大了建行卡撕破");
 		
 	}
 }
